2002de6c10d4bfa1ab92cf17a0fe39e7b3620658
Nikhil Shah <nikhilshah.com@gmail.com>
2021-09-20 20:57:44 +0530

    Update flogo_rest_api.py

--- flogo_rest_api.py
+++ flogo_rest_api.py
@@ -12,7 +12,7 @@
 7. Invoke the endpoint to "test" it """
 
 # How to run this code -
-# python3 flogo_rest_api.py <base_url> <access_token> <sourceAppId> <subscriptionLocator> <targetSubscriptionLocator> <newAppName> <App_Artifacts_Github_Path> <Override_App_Prop_Json>
+# python3 flogo_rest_api.py <api_url> <access_token> <sourceAppId> <subscriptionLocator> <targetSubscriptionLocator> <newAppName> <endpoint_path> <app_artifacts_github_path> <override_app_prop_json>
 
 import json
 import requests
@@ -22,52 +22,58 @@ import os
 
 subscriptionLocator=0
 targetSubscriptionLocator=''
-base_url=''
+api_url=''
 Auth_Header=''
 
 parser = argparse.ArgumentParser()
-parser.add_argument('base_url')
+parser.add_argument('api_url')
 parser.add_argument('access_token')
 parser.add_argument('sourceAppId')
 parser.add_argument('subscriptionLocator')
 parser.add_argument('targetSubscriptionLocator')
 parser.add_argument('newAppName')
-parser.add_argument('App_Artifacts_Github_Path')
-parser.add_argument('Override_App_Prop_Json')
+parser.add_argument('endpoint_path')
+parser.add_argument('app_artifacts_github_path')
+parser.add_argument('override_app_prop_json')
+
 args = parser.parse_args()
 
-print ('base_url :',args.base_url)
+print ('api_url :',args.api_url)
 print ('access_token :',args.access_token)
 print ('sourceAppId :',args.sourceAppId)
 print ('subscriptionLocator :',args.subscriptionLocator)
 print ('targetSubscriptionLocator :',args.targetSubscriptionLocator)
 print ('newAppName :',args.newAppName)
-print ( 'App_Artifacts_Github_Path :',args.App_Artifacts_Github_Path)
-print ('Override_App_Prop_Json:', args.Override_App_Prop_Json)
+print ('endpoint_path :',args.endpoint_path)
+print ('app_artifacts_github_path :',args.app_artifacts_github_path)
+print ('override_app_prop_json:', args.override_app_prop_json)
 
-base_url=args.base_url
+
+api_url=args.api_url
 access_token=args.access_token
 sourceAppId=args.sourceAppId
 subscriptionLocator=args.subscriptionLocator
 targetSubscriptionLocator=args.targetSubscriptionLocator
 newAppName=args.newAppName
-App_Artifacts_Github_Path=args.App_Artifacts_Github_Path
-Override_App_Prop_Json=args.Override_App_Prop_Json
+endpoint_path=args.endpoint_path
+app_artifacts_github_path=args.app_artifacts_github_path
+override_app_prop_json=args.override_app_prop_json
+
 
 
 Auth_Header={'Authorization' : 'Bearer '+access_token+'','Accept': 'application/json','User-Agent':'PostmanRuntime/7.28.3'}
 
 #Get User Info
-def get_UserInfo():
+def get_userInfo():
     try:
-        response = requests.get(base_url+'/tci/v1/userinfo', headers=Auth_Header)
-        #print('\n**** User Info*****' , response.json())  
-        #print ("*******",response.status_code)
+        response = requests.get(api_url+'/userinfo', headers=Auth_Header)
+        print('\n**** User Info*****' , response.json())  
+        print ("*******",response.status_code)
     except:
-        print ("Please enter valid base url. For eg. https://api.cloud.tibco.com for TCI US region.")
+        print ("Please enter valid API URL. For eg. https://api.cloud.tibco.com/tci/v1 for TCI US region.")
         exit()
 
-def download_app_artifacts_from_Githib(flogo_json_url,manifest_json_url):
+def download_app_artifacts_from_githib(flogo_json_url,manifest_json_url):
     #flogo_json_url = "https://raw.githubusercontent.com/nikhilshah26/TCI-FLOGO-CICD/main/Flogo_App/flogo.json"
     #manifest_json_url= "https://raw.githubusercontent.com/nikhilshah26/TCI-FLOGO-CICD/main/Flogo_App/manifest.json"
 
@@ -98,23 +104,31 @@ def pushapp_using_app_artifacts(subsLocator,appName,forceOverwrite,instanceCount
         ]
 
     print("**** Deploying app with the artifacts provided******")
-    response = requests.post(base_url+'/tci/v1/subscriptions/'+subsLocator+'/apps?appName='+appName+'&forceOverwrite='+forceOverwrite+'&instanceCount='+instanceCount+'&retainAppProps='+retainAppProps+'', headers=Auth_Header,files=files)
+    response = requests.post(api_url+'/subscriptions/'+subsLocator+'/apps?appName='+appName+'&forceOverwrite='+forceOverwrite+'&instanceCount='+instanceCount+'&retainAppProps='+retainAppProps+'', headers=Auth_Header,files=files)
     time.sleep(25)
-    print ("*****Status Code****"+str(response.status_code))
-    print(response.json())
-    resp_dict=json.loads(json.dumps(response.json()))
-    appId=resp_dict['appId']
-    print('\n***** App ID of deployed app ****' , appId)
-    return appId
+    if (response.status_code != 202):
+        print ("*****Status Code****"+str(response.status_code))
+        print (response.text)
+        exit()
+    else:
+        print ("*****Status Code****"+str(response.status_code))
+        print(response.json())
+        resp_dict=json.loads(json.dumps(response.json()))
+        appId=resp_dict['appId']
+        print('\n***** App ID of deployed app ****' , appId)
+        return appId
+
+
+    
 
 #Copy App
-def copy_App(sourceAppId,NewAppName,subscriptionLocator,targetSubscriptionLocator):
+def copy_app(sourceAppId,NewAppName,subscriptionLocator,targetSubscriptionLocator):
     print ("\n*****Copying App from Dev/QA org to Staging Org******")
     if targetSubscriptionLocator != '':
-        response = requests.post(base_url+'/tci/v1/subscriptions/'+subscriptionLocator+'/apps/'+sourceAppId+'/copy?appName='+NewAppName+'&targetSubscriptionLocator='+targetSubscriptionLocator, headers=Auth_Header)
+        response = requests.post(api_url+'/subscriptions/'+subscriptionLocator+'/apps/'+sourceAppId+'/copy?appName='+NewAppName+'&targetSubscriptionLocator='+targetSubscriptionLocator, headers=Auth_Header)
     else:
-        response = requests.post(base_url+'/tci/v1/subscriptions/0/apps/'+sourceAppId+'/copy?appName='+NewAppName, headers=Auth_Header)    
-    #print (response.status_code)
+        response = requests.post(api_url+'/subscriptions/0/apps/'+sourceAppId+'/copy?appName='+NewAppName, headers=Auth_Header)    
+    print (response.status_code)
     if (response.status_code == 401):
         print ("Invalid Secret access token. Please input valid Secret access token generated from https://account.cloud.tibco.com/manage/settings/oAuthTokens for TCI US region")
         exit()
@@ -129,68 +143,81 @@ def copy_App(sourceAppId,NewAppName,subscriptionLocator,targetSubscriptionLocato
     return appId
 
 #Get App Details
-def get_App_Details(appId,targetSubscriptionLocator):
+def get_app_details(appId,targetSubscriptionLocator):
     if targetSubscriptionLocator !='':
-        response = requests.get(base_url+'/tci/v1/subscriptions/'+targetSubscriptionLocator+'/apps/'+ appId, headers=Auth_Header) 
+        response = requests.get(api_url+'/subscriptions/'+targetSubscriptionLocator+'/apps/'+ appId, headers=Auth_Header) 
     else:
-        response = requests.get(base_url+'/tci/v1/subscriptions/0/apps/'+ appId, headers=Auth_Header) 
+        response = requests.get(api_url+'/subscriptions/0/apps/'+ appId, headers=Auth_Header) 
 
       
     print('\n**** App Details *****' , response.json())  
 
 
-def start_App(targetSubscriptionLocator,app_id):
-    req_url=base_url+'/tci/v1/subscriptions/'+targetSubscriptionLocator+'/apps/'+ app_id+'/start'
+def start_app(targetSubscriptionLocator,app_id):
+    req_url=api_url+'/subscriptions/'+targetSubscriptionLocator+'/apps/'+ app_id+'/start'
     #print ('req_url=',req_url)
     if targetSubscriptionLocator !='':
         response = requests.post(req_url, headers=Auth_Header) 
     else:
-        response = requests.post(base_url+'/tci/v1/subscriptions/0/apps/'+ app_id+'/start', headers=Auth_Header) 
+        response = requests.post(api_url+'/subscriptions/0/apps/'+ app_id+'/start', headers=Auth_Header) 
       
     print('\n**** App Started *****' , response.json())  
 
 
-def get_Endpoints(targetSubscriptionLocator,app_id):
-    if targetSubscriptionLocator !='':
-        response = requests.get(base_url+'/tci/v1/subscriptions/'+targetSubscriptionLocator+'/apps/'+ app_id+'/endpoints', headers=Auth_Header) 
-    else:
-        response = requests.get(base_url+'/tci/v1/subscriptions/0/apps/'+ app_id+'/endpoints', headers=Auth_Header) 
+def test_endpoints(targetSubscriptionLocator,app_id,path,method,body):
 
+    response = requests.get(api_url+'/subscriptions/'+targetSubscriptionLocator+'/apps/'+ app_id+'/endpoints', headers=Auth_Header) 
     resp_dict=json.loads(json.dumps(response.json()))
     req_url=resp_dict[0]['url']
-      
-    print('\n**** App Endpoints *****' , req_url+'/rest')  
-    print('\n ***** Testing the App Endpoint *******')
     time.sleep(20)
-    print('\n ***Test Endpoint Response ***',requests.get(req_url+'/rest').json())
 
-def override_App_Props(subscriptionLocator,app_id,variableType,Override_App_Prop_Json):
-    print ("***** Overriding App Prop******** ", Override_App_Prop_Json)
-    response = requests.put(base_url+'/tci/v1/subscriptions/'+subscriptionLocator+'/apps/'+ app_id+'/env/variables?variableType='+variableType, headers=Auth_Header,data=Override_App_Prop_Json)
+    if method == 'get':
+        print ("**** get req url", req_url+path)
+        resp = requests.get(req_url+path)
+        print ("*****Endpoint Response Code***",resp.status_code)
+        print ("*****Endpoint Response ***",resp.json())
+    elif method == 'post':
+        print ("**** post req url", req_url+path+body)
+        resp = requests.post(req_url+path,data=body)
+        print ("*****Endpoint Response Code***",resp.status_code)
+        print ("*****Endpoint Response ***",resp.json())
+    elif method == 'put':
+        print ("**** put req url", req_url+path+body)
+        resp = requests.put(req_url+path,data=body)
+        print ("*****Endpoint Response Code***",resp.status_code)
+        print ("*****Endpoint Response ***",resp.json()) 
+    else:
+        print (" **** Invalid method ****")
+
+def override_app_props(subscriptionLocator,app_id,variableType,override_app_prop_json):
+    print ("***** Overriding App Prop******** ", override_app_prop_json)
+    response = requests.put(api_url+'/subscriptions/'+subscriptionLocator+'/apps/'+ app_id+'/env/variables?variableType='+variableType, headers=Auth_Header,data=override_app_prop_json)
     print (response.status_code)
     print (response.text)
 
 def delete_app(subscriptionLocator,app_id):
     print ("***** Deleting app with appid: ", app_id)
-    response = requests.delete(base_url+'/tci/v1/subscriptions/'+subscriptionLocator+'/apps/'+ app_id, headers=Auth_Header)
+    response = requests.delete(api_url+'/subscriptions/'+subscriptionLocator+'/apps/'+ app_id, headers=Auth_Header)
     print (response.status_code)
     print (response.text)
 
 
 def main():
-    get_UserInfo()
-    #app_id=copy_App(sourceAppId,newAppName,subscriptionLocator,targetSubscriptionLocator)
-    download_app_artifacts_from_Githib(App_Artifacts_Github_Path+'/flogo.json',App_Artifacts_Github_Path+'/manifest.json')
+    get_userInfo()
+    #app_id=copy_app(sourceAppId,newAppName,subscriptionLocator,targetSubscriptionLocator)
+    download_app_artifacts_from_githib(app_artifacts_github_path+'/flogo.json',app_artifacts_github_path+'/manifest.json')
     app_id=pushapp_using_app_artifacts(subscriptionLocator,newAppName,"true","1","true")
-    get_App_Details(app_id,subscriptionLocator)
+    get_app_details(app_id,subscriptionLocator)
     time.sleep(30)
-    app_id_new=copy_App(app_id,newAppName,subscriptionLocator,targetSubscriptionLocator)
-    start_App(targetSubscriptionLocator,app_id_new)
+    test_endpoints(subscriptionLocator,app_id,endpoint_path,'get','')
+    
+    app_id_new=copy_app(app_id,newAppName,subscriptionLocator,targetSubscriptionLocator)
+    start_app(targetSubscriptionLocator,app_id_new)
     time.sleep(30)
-    get_Endpoints(targetSubscriptionLocator,app_id_new)
-    override_App_Props(targetSubscriptionLocator,app_id_new,'app',Override_App_Prop_Json)
+    test_endpoints(targetSubscriptionLocator,app_id_new,endpoint_path,'get','')
+    override_app_props(targetSubscriptionLocator,app_id_new,'app',override_app_prop_json)
     time.sleep(30)
-    get_Endpoints(targetSubscriptionLocator,app_id_new)
+    test_endpoints(targetSubscriptionLocator,app_id_new,endpoint_path,'get','')
     delete_app(subscriptionLocator,app_id)
     delete_app(targetSubscriptionLocator,app_id_new)
 
